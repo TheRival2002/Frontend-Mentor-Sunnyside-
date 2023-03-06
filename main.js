b336cd697414b37f87ae2b3f33c2102dc54e798f
TheRival2002 <124076661+TheRival2002@users.noreply.github.com>
2023-03-06 22:37:33 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 const mobileNav = document.querySelector(".mobile-nav");
 const primaryNav = document.querySelector(".primary-nav");
 
-mobileNav.addEventListener("click", function(){
+mobileNav.addEventListener("click", function () {
   primaryNav.toggleAttribute("data-visible");
-})
+});
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+function revealR() {
+  var reveals = document.querySelectorAll(".reveal-right");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealR);
+revealR();
