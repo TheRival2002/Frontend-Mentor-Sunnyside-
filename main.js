f7036f0655b95a647b94cf0ed119aadcd280d7c1
TheRival2002 <124076661+TheRival2002@users.noreply.github.com>
2023-05-02 10:33:40 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,69 @@
 const mobileNav = document.querySelector(".mobile-nav");
 const primaryNav = document.querySelector(".primary-nav");
 
+let counter = 31;
+const blurring = setInterval(blurBg, 30);
+
+function blurBg() {
+  counter--;
+
+  if (counter <= 0) {
+    clearInterval(blurring);
+  }
+
+  document.body.style.filter = `blur(${counter}px)`;
+}
+
 mobileNav.addEventListener("click", function () {
   primaryNav.toggleAttribute("data-visible");
 });
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 50) {
+    primaryNav.removeAttribute("data-visible");
   }
+
+  const reveals = document.querySelectorAll(".reveal");
+  slideIn(reveals);
+});
+
+function slideIn(reveals) {
+  reveals.forEach((reveal, index) => {
+    if (index === 1) {
+      reveal.classList.add("reveal-right");
+    }
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[index].getBoundingClientRect().top;
+    const elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[index].classList.add("active");
+    } else {
+      reveals[index].classList.remove("active");
+    }
+  });
 }
 
-window.addEventListener("scroll", reveal);
-reveal();
+// letter animation on scroll
 
-function revealR() {
-  var reveals = document.querySelectorAll(".reveal-right");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
+const title = document.querySelector(".letter-animate");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+title.addEventListener("mouseover", () => {
+  let titleContent = title.textContent.split("");
+  let counter = 0;
+  const titleValue = title.dataset.value;
+  let letterAnimation = setInterval(() => {
+    counter += 1 / 3;
+    titleContent = titleContent.map((letter, index) => {
+      if (index < counter - 1) {
+        return titleValue[index];
+      }
+      return letters[Math.floor(Math.random() * 26)];
+    });
+    title.textContent = titleContent.join("");
+    if (counter > titleValue.length) {
+      clearInterval(letterAnimation);
+      title.textContent = titleValue;
     }
-  }
-}
-
-window.addEventListener("scroll", revealR);
-revealR();
+  }, 30);
+});
